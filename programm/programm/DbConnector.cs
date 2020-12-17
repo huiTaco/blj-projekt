c74e44e691fd73db045453b8d782df6f02bd32eb
Thierry Loetscher <thierry.loetscher@komaxgroup.com>
2020-12-17 10:58:39 +0100

    initial commit

---
+++ programm/programm/DbConnector.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace programm
+{
+    public class DbConnector
+    {
+        SqlConnection dbConnection = new SqlConnection("Server=CH10LT1284\\SQLEXPRESS;Database=blj-projectwpf;Trusted_Connection=True;");
+
+        public SqlConnection GetConnection()
+        {
+            try
+            {
+                if (dbConnection.State != ConnectionState.Open)
+                    dbConnection.Open();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Verbindung zur Datenbank konnte nicht aufgebaut werden: " + ex.Message);
+            }
+           return dbConnection;
+        }
+        public DataTable GetDataFromDb(string query)
+        {
+            ValidateConnection();
+
+            DataTable table = new DataTable();
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = dbConnection;
+            cmd.CommandText = query;
+
+            SqlDataAdapter adap = new SqlDataAdapter();
+            adap.SelectCommand = cmd;
+            adap.Fill(table);
+
+            return table;
+        }
+
+        public void InsertHusi(string fach, string deinehusi, DateTime erledigtBis)
+        {
+            ValidateConnection();
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = dbConnection;
+                cmd.CommandText = "insert tbHusiplaner(Fach, DeineHusi, ErledigtBis) values (@fach, @deinehusi, @datum)";
+
+                cmd.Parameters.AddWithValue("@fach", fach);
+                cmd.Parameters.AddWithValue("@deinehusi", deinehusi);
+                cmd.Parameters.AddWithValue("@datum", erledigtBis);
+                
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Die Husi konnte nicht in die Datenbank eingetragen werden." + ex.Message);
+            }
+        }
+        private void ValidateConnection()
+        {
+            try
+            {
+                if (dbConnection.State != ConnectionState.Open)
+                    dbConnection.Open();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Datenbankverbindung konnte nicht geöffnet werden: " + ex.Message);
+            }
+        }
+    }
+}
+
---
+++ programm/programm/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace programm
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DbConnector connector = new DbConnector();
+
+        private void LoadData()
+        {
+
+            DataTable data = connector.GetDataFromDb("select * from tbHusiplaner");
+
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadData();
+           // textausgabe.Text = "Dies ist die Liste von den Hausaufgaben die du noch machen muss.\nFalls du die Aufgabe erledigt hat kreuze si an und sie verschwindet wieder ";
+        }   
+        public class Husiplaner
+        {
+            public string Fach { get; set; }
+            public string Husi { get; set; }
+            public string Datum { get; set; }
+            public string Erledigt { get; set; }
+
+
+        }
+            
+
+        private void btnHome_Click(object sender, RoutedEventArgs e)
+        {
+            tabCtrl.SelectedIndex = 0;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            tabCtrl.SelectedIndex = 1;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            tabCtrl.SelectedIndex = 2;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            tabCtrl.SelectedIndex = 3;
+        }
+
+        private void btnFinish_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Finish wurde gedrückt.");
+            string fach = txtBox_fach.Text;
+            string deinehusi = txtBox_deinehusi.Text;
+            DateTime date = datepicker.DisplayDate;
+
+            Lable1.Content = fach + "\n" + deinehusi + "\n" + date;
+
+            connector.InsertHusi(fach, deinehusi, date);
+          
+            txtBox_fach.Clear();
+            txtBox_deinehusi.Clear();
+            datepicker.SelectedDate = null;
+        }
+    }
+}
